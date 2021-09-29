4104e92f91dfd6b59300d737c626d52417656988
Edouard Misset <edouardmisset@gmail.com>
2021-09-29 17:06:09 +0200

    converted to arrow fct

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,16 @@
 import './Homepage.scss'
 
-export default function Homepage() {
-  return (
-    <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+)
+
+export default Homepage
