0c6cbd7ba3f2db421eb6ff0065e01b15111f59e6
Edouard Misset <edouardmisset@gmail.com>
2021-09-30 01:34:44 +0200

    Added a collection-preview component and a shop page to display the different collections

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/homepage/Homepage'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
+import Shop from './pages/shop/Shop'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop/" component={Shop} />
       </Switch>
     </div>
   )
