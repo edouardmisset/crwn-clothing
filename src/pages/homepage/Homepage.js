dc21f5d160d43628675f820e37919fa2d09acdb3
Edouard Misset <edouardmisset@gmail.com>
2021-09-29 17:36:44 +0200

    Some more components & architecture...

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,10 @@
+import Directory from '../../components/directory/Directory'
+import './Homepage.scss'
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+)
+
+export default Homepage
