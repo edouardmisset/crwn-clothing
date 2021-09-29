89d7edecd0c2028f69fcd6220fbfe7addadbe1c3
Edouard Misset <edouardmisset@gmail.com>
2021-09-29 17:54:54 +0200

    some styling to the menu items and directory

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -46,13 +46,13 @@ export class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+        {this.state.sections.map(({ id, title, imageUrl, size, linkUrl }) => (
           <MenuItem
             key={id}
             title={title}
-            // imageUrl={imageUrl}
-            // size={size}
-            // linkUrl={linkUrl}
+            imageUrl={imageUrl}
+            size={size}
+            linkUrl={linkUrl}
           />
         ))}
       </div>
