21d38240df3fdfd27dde9fb97995882075b60fd3
Edouard Misset <edouardmisset@gmail.com>
2021-10-05 17:36:28 +0200

    Signin and register component (only signin displayed for now) Register soon to come

---
+++ src/pages/signin-and-register-page/signin-and-register.js
@@ -0,0 +1,10 @@
+import Signin from '../../components/signin/Signin'
+import './signin-and-register.scss'
+
+export default function signinAndRegister() {
+  return (
+    <div className='sign-in-and-register'>
+      <Signin />
+    </div>
+  )
+}
