7019a8ad3702b84061fa39210bf941d4c4e5ad7f
Edouard Misset <edouardmisset@gmail.com>
2021-10-05 17:35:16 +0200

    Empty register component

---
+++ src/components/register/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Register.scss'
+
+export class Register extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Register
