88f3447ef40e3f333771de6a6762d2030d6af954
Edouard Misset <edouardmisset@gmail.com>
2021-10-07 13:03:08 +0200

    Changed App functional component to class component and setting the logged current user to the state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/homepage/Homepage'
 import Shop from './pages/shop/Shop'
 import Header from './components/header/Header'
 import signinAndRegister from './pages/signin-and-register-page/signin-and-register'
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/signin" component={signinAndRegister} />
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+      console.log(this.state.currentUser)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/signin" component={signinAndRegister} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
