3fda0846f4efbe7f83411ac3efedff5b6677e214
Edouard Misset <edouardmisset@gmail.com>
2021-10-12 21:39:33 +0200

    Populating Collection Page

--- src/pages/collection/CollectionPage.js
+++ src/pages/collection/CollectionPage.js
@@ -3,12 +3,19 @@ import CollectionItem from '../../components/collection-item/CollectionItem'
 import { selectCollection } from '../../redux/shop/shopSelector'
 import './CollectionPage.scss'
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection-page">
-    <h2 className="title">COLLECTION PAGE</h2>
-    {/* <div>{match.params.collectionId}</div> */}
-  </div>
-)
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+        </div>
+    </div>
+  )
+}
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
