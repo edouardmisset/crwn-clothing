767cc006ea22ab5cb603056899fe7d0594553bff
Edouard Misset <edouardmisset@gmail.com>
2021-10-12 09:54:21 +0200

    Added a add to cart button and some style

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,3 +1,4 @@
+import CustomButton from '../custom-button/CustomButton'
 import './CollectionItem.scss'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -12,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 )
 
