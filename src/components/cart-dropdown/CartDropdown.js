6f2b7c0bc58e9bcfbf969cee6d750751f10efa85
Edouard Misset <edouardmisset@gmail.com>
2021-10-12 14:08:47 +0200

    Dealt with the empty cart case

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,18 +1,22 @@
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import { selectCartItems } from '../../redux/cart/cartSelectors'
 import CartItem from '../cart-item/CartItem'
-import { createStructuredSelector } from 'reselect'
-
 import CustomButton from '../custom-button/CustomButton'
 import './CartDropdown.scss'
+
 
 const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
