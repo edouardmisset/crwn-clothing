880d4ce9380eb81fff2177738246963d43dcba17
Edouard Misset <edouardmisset@gmail.com>
2021-10-12 19:04:46 +0200

    Added shopReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,8 +3,8 @@ import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import cartReducer from './cart/cartReducer'
 import directoryReducer from './directory/directoryReducer'
+import shopReducer from './shop/shopReducer'
 import userReducer from './user/userReducer'
-
 
 const persistConfig = {
   key: 'root',
@@ -16,6 +16,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 })
 
 export default persistReducer(persistConfig, rootReducer) // persistReducer is a function that takes in persistConfig and rootReducer
