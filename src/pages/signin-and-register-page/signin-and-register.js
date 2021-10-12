f1db74b879a5046a159f1d48e8a4a9a8aaeaf25a
Edouard Misset <edouardmisset@gmail.com>
2021-10-12 08:57:14 +0200

    typo

--- src/pages/signin-and-register-page/signin-and-register.js
+++ src/pages/signin-and-register-page/signin-and-register.js
@@ -2,7 +2,7 @@ import Register from '../../components/register/Register'
 import Signin from '../../components/signin/Signin'
 import './signin-and-register.scss'
 
-export default function signinAndRegister() {
+export default function SigninAndRegister() {
   return (
     <div className="sign-in-and-register">
       <Signin />
