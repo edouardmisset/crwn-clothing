af26dabcac0be539ea03b0673f0bbed4b3601601
Edouard Misset <edouardmisset@gmail.com>
2021-10-14 15:35:54 +0200

    Added a warning to get a mocked card deetails to be able to test the functionnality

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -34,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/
+      {(new Date().getFullYear() + 1).toString().slice(2, 4)} - CVC: 123
+    </div>
     <StripeButton price={total} />
   </div>
 )
